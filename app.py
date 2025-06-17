3fbefd678c6fbf209962f04358c0f63e0b4a9f8c
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2025-06-17 11:17:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import cv2
-import cvzone
 from cvzone.SelfiSegmentationModule import SelfiSegmentation
 import os
 
@@ -7,10 +6,26 @@ cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
 segmentor = SelfiSegmentation()
-imgBg = cv2.imread("Images/img1.jpg")
-imgBg = cv2.resize(imgBg,(640,480))
+
+listImg = os.listdir("Images")
+imgList = []
+for imgPath in listImg:
+    img = cv2.imread(f"Images/{imgPath}")
+    img = cv2.resize(img, (640, 480))
+    imgList.append(img)
+
+indexImg = 0
+
 while True:
     success, img = cap.read()
-    img = segmentor.removeBG(img,imgBg,cutThreshold=0.7)
+    img = segmentor.removeBG(img,imgList[indexImg],cutThreshold=0.6)
     cv2.imshow("Camera Feed",img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord("a"):
+        if indexImg > 0:
+            indexImg -= 1
+    elif key == ord("d"):
+        if indexImg < len(imgList) - 1:
+            indexImg += 1
+    elif key == ord("q"):
+        break
